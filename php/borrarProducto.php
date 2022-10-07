38c4dfc44134713f3ae4fbfb2c60be35464a54ab
JuanelopS <contacto@juangavira.me>
2022-10-07 20:13:15 +0200

    borrarProducto.php: pendiente borrado de archivo

--- php/borrarProducto.php
+++ php/borrarProducto.php
@@ -13,23 +13,25 @@
 
     // borrar archivo de imagen del directorio ?
 
-    $path = $_SERVER['DOCUMENT_ROOT'] . '/img/';
+    // $path = $_SERVER['DOCUMENT_ROOT'] . '\img\\';
 
-    $item_query = "SELECT nombre FROM items WHERE id = $item_id";
+    // $item_query = "SELECT * FROM items WHERE id = $item_id";
 
-    $item_connect = $pdoConnection->prepare($item_query);
-    $item_connect->execute();
-    $item_result = $item_connect->fetchAll(PDO::FETCH_ASSOC);
+    // $item_connect = $pdoConnection->prepare($item_query);
+    // $item_connect->execute();
+    // $item_result = $item_connect->fetchAll(PDO::FETCH_ASSOC);
+    
+    // $nombre_item = $item_result['nombre'];
 
-    $dest_path = $path . $item_result['nombre'];
+    // $dest_path = $path . $nombre_item;
 
-    // Si el archivo existe, borrarlo
-    if(file_exists('$dest_path')) {
-        unlink('$dest_path');
-        echo "hola";
-    }
+    //   // Si el archivo existe, borrarlo
+    //   if(file_exists('$dest_path')) {
+    //       unlink('$dest_path');
+    //   }
 
-    header("Location: admin.php");
+    //   // header("Location: admin.php");
+    //   echo $dest_path;
 
   } catch (Exception $err){
   print "Error!: " . $err->getMessage() . "<br>";
